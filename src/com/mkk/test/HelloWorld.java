ef6831e19fac327e323bda032755f72036fe3e6a
maokk <maokangkai@163.com>
2016-01-21 09:28:12 +0800

    添加了一个main函数

---
+++ src/com/mkk/test/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.mkk.test;
+
+/**
+ * 
+ * 测试eclipse到github工程
+ * @author 阿术
+ *
+ */
+public class HelloWorld {
+	public HelloWorld(){
+		System.out.println("测试github项目是否可以同eclipse相互导");
+	}
+	public static void main(String[] args){
+		new HelloWorld();
+	}
+}
