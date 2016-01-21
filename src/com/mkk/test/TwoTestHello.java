aea43960ee605018ac2ba3d1e0a976983df4cf6d
maokk <maokangkai@163.com>
2016-01-21 23:11:39 +0800

    添加了一个新的测试类

---
+++ src/com/mkk/test/TwoTestHello.java
@@ -0,0 +1,18 @@
+package com.mkk.test;
+
+public class TwoTestHello {
+
+	public void a(){
+		
+	}
+	public void b(){
+		
+
+	}
+	
+	public void c(){
+		a();
+		b();
+		System.out.println("调用成功与否");
+	}
+}
